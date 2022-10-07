dc265508f4a34bc9ee17f30ef0db30201bd2f907
Garrett Adams <Garrett Adams>
2022-10-06 23:57:25 -0700

    added axios library that gets quote from endpoint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from "react";
 
+// const baseURL = "http://127.0.0.1:8000/garrett/"
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1"
 function App() {
+
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if(!post) return null;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {post.body}
         </p>
         <a
           className="App-link"
