02573620c8d2f87530109b5e17dbb1102c6a0979
Alfred Zhuang <alfredzhuang123@gmail.com>
2022-10-18 16:49:42 -0700

    Make call to backend to get a product using its id

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,32 @@
 import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Placeholder from "../images/placeholder.svg";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Product = () => {
+  const [product, setProduct] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
   const { id } = useParams();
+  const url = `http://127.0.0.1:8000/product/${id}/?format=json`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  else if (errorMessage) return <div>{errorMessage}</div>;
+
   return (
     <div>
       This is the page for the product with an id of {id}
@@ -14,11 +37,13 @@ const Product = () => {
           </Col>
           <Col>
             <Container>
-              <Row>Product name/price</Row>
+              <Row>
+                {product.product_name} | {product.price}
+              </Row>
               <Row>Shop name</Row>
               <Row>Rating</Row>
               <Row>Add to cart</Row>
-              <Row>Description</Row>
+              <Row>{product.description}</Row>
               <Row>Extra Information</Row>
             </Container>
           </Col>
