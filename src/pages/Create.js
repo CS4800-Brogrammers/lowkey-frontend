ea98a7e6983d89e14d245b4f45a5ec674d80dd19
lnguyen0103 <lamnnguyen23@gmail.com>
2022-10-24 13:49:57 -0700

    Basic Form w/o button functionality

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,44 @@
 import axios from "axios";
 import React from "react";
+import {Container, Row, Col, Button} from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form'
+import { InputGroup } from "react-bootstrap";
 
 const Create = () => {
   return (
-    <h1>Work in progress, Create Page</h1>
+    <div>
+      <Container>
+        
+        <Row>
+          <Col>Image should go here
+
+          </Col>
+          <Col>
+            <Form>
+              <Form.Group controlId="formProdName">
+                <Form.Label> Product Name</Form.Label>
+                <Form.Control as= "input" placeholder="Enter product name" />
+              </Form.Group>
+              <Form.Label> Product Price </Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>$</InputGroup.Text>
+                <Form.Control aria-label="formProdPrice" />
+              </InputGroup>
+              <Form.Group controlId="formProdDescr">
+                <Form.Label> Product Description </Form.Label>
+                <Form.Control as="textarea" aria-label="With textarea"  />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Create
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+
+      </Container>
+      
+    </div>
   );
 };
 
