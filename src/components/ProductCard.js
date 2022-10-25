6d73018820c7038d00d005b32c256049852269c8
Garrett Adams <Garrett Adams>
2022-10-24 20:37:38 -0700

    product card connected to backend

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {Container, Row, Col} from "react-bootstrap";
 import StarRating from './StarRating'
 
-const ProductCard = () => {
+const ProductCard = (props) => {
 
 
     return( 
@@ -14,18 +14,18 @@ const ProductCard = () => {
             <Card.Img variant = "top" src = {img1}/>
             <Card.Body>
                 <Container className = "mt-100">
-                    <Card.Title>Chocolate Cake</Card.Title>
+                    <Card.Title>{props.title}</Card.Title>
                     <Row>
                         <Col>
-                            <Card.Text>$21.99</Card.Text>
+                            <Card.Text>{props.price}</Card.Text>
                         </Col>
                         <Col>
                             <StarRating/>
                         </Col>
                     </Row>
-                    <Card.Text>Garrett's Bakery</Card.Text>
+                    <Card.Text>{props.shop}</Card.Text>
                 </Container>
-                <a href = "/product/:id" className = "stretched-link" data-testid = "link"></a>
+                <a href = "/product/${props.id}" className = "stretched-link" data-testid = "link"></a>
             </Card.Body>
         </Card>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {Container} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProductCard from "../components/ProductCard";
+
+
 const Home = () => {
-  return <ProductCard/>;
+  const [id, setId] = useState(null);
+  const [productName, setProductName] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [rating, setRating] = useState(null);
+  const [shop, setShop] = useState(null);
+
+  const productsURL = "http://127.0.0.1:8000/product/1/?format=json";
+  useEffect(() => {
+    axios.get(productsURL).then((response) => {
+      setId(response.data['id']); 
+      setProductName(response.data['product_name']);
+      setPrice(response.data['price']);
+      setRating("5");
+      setShop("Garrett's Bakery");
+    });
+  }, []);
+  
+  return(
+    <Container> 
+      <ProductCard id = {id} title = {productName} price = {price} rating = {rating} shop = {shop}/>
+    </Container>
+
+    
+  );
+  
 };
 
 export default Home;
