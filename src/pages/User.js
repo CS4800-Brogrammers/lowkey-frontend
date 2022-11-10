aaafccc0a333efcda2e498f6191fc73edf7afa7b
Alfred Zhuang <alfredzhuang123@gmail.com>
2022-11-09 19:02:29 -0800

    Add a logout button to the user page and redirect user to login page if not logged in

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,32 @@
+import { useContext, useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
 const User = () => {
-  return <div>This is the user page</div>;
+  const navigate = useNavigate();
+  const { user, setUser, setAuthTokens } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (!user) navigate("/login");
+  });
+
+  return (
+    <Container>
+      <Row>This is the user page</Row>
+      <Row>
+        <Button onClick={handleSubmit}>Logout</Button>
+      </Row>
+    </Container>
+  );
 };
 
 export default User;
