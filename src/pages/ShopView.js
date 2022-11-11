809c598e90a6d2babc9fc5af62f4fd6e991d3efb
Garrett Adams <Garrett Adams>
2022-11-10 23:30:44 -0800

    changed route to CreateShop

---
+++ src/pages/ShopView.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect, useRef, useMemo} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Form, Button, Container, Row} from 'react-bootstrap'
+import "./ShopView.css";
+import axios from 'axios';
+
+const ShopView = () => {
+    
+
+    return(
+        <Container>
+            <Row></Row>
+        </Container>
+    )
+}
